2ab178ff70e628b8fdcec04f2f1c084ae2e04d16
Mohamed Magdy <mohammedmagdy6522@gmail.com>
2024-03-17 02:32:21 +0200

    Add some details

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,8 +4,13 @@ import Footer from '../Compnents/Footer';
 import DisplayProducts from '../Compnents/DisplayProducts';
 import MainBox from '../Compnents/MainBox';
 import Navbar from '../Compnents/Navbar';
+import { useEffect } from 'react';
 
 function Home() {
+  useEffect(() => {
+    // Scroll to the top of the page when the component mounts (page loads)
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div>
       <Navbar/>
--- src/Pages/Order.jsx
+++ src/Pages/Order.jsx
@@ -1,8 +1,13 @@
+import { useEffect } from "react";
 import Footer from "../Compnents/Footer";
 import Navbar from "../Compnents/Navbar";
 import ProductsDetails from "../Compnents/ProductsDetails";
 
 function Order() {
+  useEffect(() => {
+    // Scroll to the top of the page when the component mounts (page loads)
+    window.scrollTo(0, 0);
+  }, []);
     return (
       <div className="">
         <Navbar/>
